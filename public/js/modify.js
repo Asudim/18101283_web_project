21dc3c6960572d2cbde12ced1aed2aa1797819c7
Asudim <ghlals0127@gmail.com>
2023-12-05 09:33:46 +0900

    Add files via upload

--- public/js/modify.js
+++ public/js/modify.js
@@ -1,1 +1,11 @@
 // 원본 게시글 로드 후 수정 삭제 기능 제공
+
+const express = require("express")
+const router = express.Router();
+
+const { sql_login, sql_board } = require('../../home');
+
+// loopkup에서 받아온 정보에 대해 sql 수행
+
+// text박스에 기존 text와 title 불러오기 후 수정 기능
+// 이후 저장
